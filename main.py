8b71c89beb1144016e1a0d300133f232073a8c4f
GabrieelAndrade <105891716+GabrielAndradeNas@users.noreply.github.com>
2022-05-25 19:05:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, flash
+app = Flask('app')
+
+app.config['SECRET_KEY'] = 'MoWRscvAbghlkP'
+
+@app.route('/')
+def index():
+  return redirect('/flashing')
+
+@app.route('/flashing')
+def flashing():
+  flash('Sucesso', 'success')
+  return render_template('flashing.html')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
